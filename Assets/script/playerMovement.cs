5cd087534932c9aaf8f81bd7530ef12ed1d22033
Sisi Lee <1581618775@qq.com>
2019-04-24 22:08:45 +0800

    墙体加了刚体，text和icon

--- Assets/script/playerMovement.cs
+++ Assets/script/playerMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class playerMovement : MonoBehaviour
@@ -7,10 +8,15 @@ public class playerMovement : MonoBehaviour
     public Rigidbody rb;
     public float forwardSpeed = 2000f;
     public float sidewayForce = 500f;
+    private int count;
 
     // Start is called before the first frame update
 
     // Update is called once per frame
+    private void Start()
+    {
+        count = 0;
+    }
     void FixedUpdate()
     {
         rb.AddForce(0, 0, forwardSpeed * Time.deltaTime);
@@ -20,6 +26,14 @@ public class playerMovement : MonoBehaviour
         }else if (Input.GetKey("a"))
         {
             rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("icon"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
         }
     }
 }
