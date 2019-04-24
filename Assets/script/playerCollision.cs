e0a3457252786a906cf945ed9c07a2ae7b855a23
Sisi Lee <1581618775@qq.com>
2019-04-24 20:23:35 +0800

    game template

---
+++ Assets/script/playerCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+    public playerMovement movement;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "obstacle")
+        {
+            movement.enabled = false; 
+        }
+    }
+}
---
+++ Assets/script/playerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardSpeed = 2000f;
+    public float sidewayForce = 500f;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardSpeed * Time.deltaTime);
+        if (Input.GetKey("d")) 
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0);
+        }else if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
